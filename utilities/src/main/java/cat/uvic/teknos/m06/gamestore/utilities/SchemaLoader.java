2667cb7e2b95c982869bc0bb55c296deca784992
Aldo Matias Lazo Rocha <aldomatias.lazo@uvic.cat>
2022-04-25 19:42:07 +0200

    Created new interface Schemaloader and new package exceptions

---
+++ utilities/src/main/java/cat/uvic/teknos/m06/gamestore/utilities/SchemaLoader.java
@@ -0,0 +1,9 @@
+package cat.uvic.teknos.m06.gamestore.utilities;
+
+/***
+ * Works with sql files
+ */
+
+public interface SchemaLoader {
+    void load();
+}
---
+++ utilities/src/main/java/cat/uvic/teknos/m06/gamestore/utilities/exceptions/SchemaLoaderException.java
@@ -0,0 +1,4 @@
+package cat.uvic.teknos.m06.gamestore.utilities.exceptions;
+
+public class SchemaLoaderException  extends RuntimeException{
+}
